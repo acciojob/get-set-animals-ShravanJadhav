f259c9b0cc5811bdb3fd37e218d55359e49482fb
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-07-12 21:27:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,6 +26,21 @@ class Cat extends Animal {
 }
 
 
+const animal = new Animal("Unknown");
+console.log(animal.species); 
+animal.makeSound(); 
+
+const cat = new Cat("Cat");
+console.log(cat.species); 
+cat.makeSound(); 
+cat.purr();
+
+const dog = new Dog("Dog");
+console.log(dog.species); 
+dog.makeSound(); 
+dog.bark(); 
+
+
 
 // Do not change the code below this line
 window.Animal = Animal;
